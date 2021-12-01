ca3949a2765becd63992d2fbbd4f65ac1e5af8f0
Chen Yuyou <chenyuyou@126.com>
2021-12-01 17:38:59 +0800

    big modify

--- questionnaire/__init__.py
+++ questionnaire/__init__.py
@@ -23,8 +23,9 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
     male = models.StringField()
-    year_month = models.StringField()
     identification = models.StringField()
+    nationality = models.StringField()
+    nationality_other = models.StringField()
     birth_province=models.StringField()
     birth_city=models.StringField()
     birth_district=models.StringField()
@@ -50,47 +51,53 @@ class Player(BasePlayer):
     provincial_ranking=models.StringField()
     household_income = models.StringField()
     brother_sister = models.IntegerField()
+    father_age = models.IntegerField()
+    mother_age = models.IntegerField()
 
-   
-    fuage = models.IntegerField(label="13、您父亲的年龄？",min=0)
-    muage = models.IntegerField(label="14、您母亲的年龄？",min=0)
-    fumuhukoudi=models.StringField(label="15、您父母的户口登记地？请按照xx省xx市xx区（县）填写")
-    hukouleixing = models.StringField(label="16、您父母的户口类型？",choices=["农业户口","非农户口"])
-    fuedu = models.StringField(label="17、您父亲的教育水平？",choices=["未受过教育","小学","初中","高中","中专/技校","职业高中","大学专科","大学本科","研究生及以上"])
-    muedu = models.StringField(label="18、您母亲的教育水平？",choices=["未受过教育","小学","初中","高中","中专/技校","职业高中","大学专科","大学本科","研究生及以上"])
-    fuwork = models.StringField(label="19、您父亲的工作类型？",choices=["国家机关工作人员","企业中高级管理人员","教师、工程师、医生、律师","技术工人（包括司机）","生产制造业职工","商业与服务业职工","个体户","农民","渔民","无业、失业、下岗"])
-    muwork = models.StringField(label="20、您母亲的工作类型？",choices=["国家机关工作人员","企业中高级管理人员","教师、工程师、医生、律师","技术工人（包括司机）","生产制造业职工","商业与服务业职工","个体户","农民","渔民","无业、失业、下岗"])
+
+    family_province =models.StringField()
+    family_city = models.StringField()
+    family_district = models.StringField()
+    hukou_type=models.StringField()
+    father_edu=models.StringField()
+    mother_edu=models.StringField()
+    father_work=models.StringField()
+    father_work_other=models.StringField()
+    mother_work=models.StringField()
+    mother_work_other=models.StringField()
+    interests = models.StringField()
+    societies_name = models.StringField()
+    societies_feature = models.StringField()
+    party_member = models.StringField()
+    class_leader = models.StringField()
+    student_union = models.StringField()
+    death_risk = models.StringField()
+    typhoon_risk = models.StringField()   
     
     
-    xingquaihao = models.StringField(label="21、您最大的兴趣爱好？")
-    shetuan = models.StringField(label="22、您参加的社团名称？")
-    shetuantezheng = models.StringField(label="23、您参加的社团具有哪些特征？",choices=["团队合作和集体主义要求高与个人表现（主义）","个人表现（主义）胜过团队合作和集体主义","两者都强调"])
-    dangyuan = models.StringField(label="24、您是党员么？",choices=["是","否"])
-    ganbu = models.StringField(label="25、您是否为学生干部？",choices=["是","否"])
-    xueshenghui = models.StringField(label="26、您是校（院）学生会成员么？",choices=["是","否"])
-    ziranzaihai = models.StringField(label="27、您家乡面临的自然死亡风险是什么？",choices=["地震","水灾","台风","雪灾"])
-    taifengpinlv = models.StringField(label="28、您上大学前经历的台风多么？",choices=["没有","较少，几年才一次","一年一次","一年两次","一年三次","一年四次及以上"])
-    xinqing = models.StringField(label="29、您今天的心情怎么样？",choices=["非常开心","开心","不好不坏","不开心","非常不开心"])
-    yumen = models.StringField(label="30、过去一周，你感到沮丧、郁闷，做什么事情都不能振奋的频率？",choices=["几乎每天","有四五天","两三天","有一天","从来没有过"])
-    jinzhang = models.StringField(label="31、过去一周，你感到精神紧张的频率？",choices=["几乎每天","有四五天","两三天","有一天","从来没有过"])
-    zuolibuan = models.StringField(label="32、过去一周，你感到坐卧不安、难以保持平静的频率？",choices=["几乎每天","有四五天","两三天","有一天","从来没有过"])
-    meixiwang = models.StringField(label="33、过去一周，你感到未来没有希望的频率？",choices=["几乎每天","有四五天","两三天","有一天","从来没有过"])
-    kunnan = models.StringField(label="34、过去一周，你做任何事情感到困难的频率？",choices=["几乎每天","有四五天","两三天","有一天","从来没有过"])
-    shenghuoyiyi = models.StringField(label="35、过去一周，您认为生活没有意义的频率？",choices=["几乎每天","有四五天","两三天","有一天","从来没有过"])
+    
+
+    feeling = models.StringField()
+    frustrated = models.StringField()
+    nervous = models.StringField()
+    not_calm = models.StringField()
+    hopeless = models.StringField()
+    difficulty = models.StringField()
+    no_meaning = models.StringField()
      
 
 
 
-    changyongshou = models.StringField(label="36、您的常用手？",choices=["左手","右手"])
-    jianzhi = models.StringField(label="37、您是否曾经兼职过？",choices=["是","否"])
-    major = models.StringField(label="38、您的专业是？")
-    xiaofei = models.IntegerField(label="38、您月消费？")
-    xianxue = models.StringField(label="39、您是否主动献血过？",choices=["是","否"])
-    xianxuetime = models.IntegerField(label="40、您的献血次数？",min=0)
-    juankuan = models.StringField(label="41、您是否主动捐款过？",choices=["是","否"])
-    experience = models.StringField(label="42、您是否参加过类似实验？",choices=["是","否"])
-    shehuidiwei = models.IntegerField(label="43、如果社会最顶层用10表示，最底层用1表示，您认为自己属于什么社会位置？", min=1,max=10)
-    banjidiwei = models.IntegerField(label="44、班级中，如果最顶层用10表示，最底层用1表示，您认为自己属于什么班级位置？", min=1,max=10)
+    dominant_hand = models.StringField()
+    part_time = models.StringField()
+    major = models.StringField()
+    consumption = models.IntegerField()
+    donate_blood = models.StringField()
+    donate_blood_num = models.IntegerField()
+    donate = models.StringField()
+    experiment = models.StringField()
+    social_status = models.IntegerField()
+    class_status = models.IntegerField()
 
 
 class MyPage(Page):
@@ -102,10 +109,12 @@ class MyPage(Page):
 
 class QPage(Page):
     form_model = 'player'
-    form_fields = ['brother_sister']
-#   form_fields = ['male','year_month','identification','birth_province','birth_city','birth_district','hukou_province','hukou_city','hukou_district','three_province','three_city','three_district','primary_province',
-#    'primary_city','primary_district','junior_province','junior_city','junior_district','high_province','high_city','high_district','language_score','math_score','english_score','other_score','provincial_ranking',
-#    'household_income']
+#    form_fields = ['male','identification','nationality','nationality_other']
+    form_fields = ['male','identification','nationality','nationality_other','birth_province','birth_city','birth_district','hukou_province','hukou_city','hukou_district','three_province','three_city','three_district','primary_province',
+    'primary_city','primary_district','junior_province','junior_city','junior_district','high_province','high_city','high_district','language_score','math_score','english_score','other_score','provincial_ranking',
+    'household_income','brother_sister','father_age','mother_age','family_province','family_city','family_district','hukou_type','father_edu','mother_edu','father_work','father_work_other','mother_work','mother_work_other',
+    'interests','societies_name','societies_feature','party_member','class_leader','student_union','death_risk','typhoon_risk','feeling','frustrated','nervous','not_calm','hopeless','difficulty','no_meaning',
+     'dominant_hand','part_time','major','consumption','donate_blood','donate_blood_num','donate','experiment','social_status','class_status']
 
 class ResultsWaitPage(WaitPage):
     pass
